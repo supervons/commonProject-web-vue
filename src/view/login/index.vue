36efe3c2f85c8c7e02f97081af3848289f1a5580
supervons <supervons@sina.com>
2019-08-29 17:43:13 +0800

    增加登录页面
    add login page

---
+++ src/view/login/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-row class="content">
+      <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="12">
+        <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+          <el-form-item label="用户名" prop="userName">
+            <el-input v-model="form.userName" placeholder="请输入用户名" prefix-icon="el-icon-user"/>
+          </el-form-item>
+          <el-form-item label="密码" prop="passWord">
+            <el-input v-model="form.passWord" placeholder="请输入密码" prefix-icon="el-icon-coordinate" show-password/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit('form')">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'commonProject 管理台',
+      form: {
+        userName: '',
+        passWord: ''
+      },
+      rules: {
+        userName: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        passWord: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .content {
+    justify-content: center;
+    display: flex;
+  }
+</style>
