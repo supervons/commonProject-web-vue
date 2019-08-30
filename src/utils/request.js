4b68572eb770a8db7dfb9afcec660b0a6898a3a7
supervons <supervons@sina.com>
2019-08-30 11:50:55 +0800

    增加注释
    Add comment

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,11 @@
+/**
+ * 基于 axios 的通用网络请求，可以进行参数的封装以及对请求、响应的拦截
+ * superfys 2019/08/30
+ */
 import axios from 'axios'
 import {Message, MessageBox} from 'element-ui'
 
-// 创建axios实例
+// 创建 axios 实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api的base_url
   timeout: 15000, // 请求超时时间
@@ -12,7 +16,7 @@ const service = axios.create({
   }
 })
 
-// request拦截器
+// request 拦截器
 service.interceptors.request.use(config => {
   // if (store.getters.token) {
   //   config.headers['Authorization'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
@@ -24,7 +28,7 @@ service.interceptors.request.use(config => {
   return Promise.reject(error)
 })
 
-// respone拦截器
+// response 拦截器
 service.interceptors.response.use(
   response => {
     /**
