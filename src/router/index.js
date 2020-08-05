9e9d895caa6885f01d3c4ef1c70f7622e9c3cf8a
supervons <supervons@sina.com>
2020-08-06 00:16:41 +0800

    1，增加hello-world测试路由；
    1.add hello-world test router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/view/login'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/hello-world',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
